551d4145f0aeff2baac3dd3afa9301e2f3180131
sonali1993 <sonali.dheple@gmail.com>
2018-10-16 11:34:59 +0100

    Updates

---
+++ precache-manifest.8a2c0a32c017e70651c952a950c1732e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "86b5c5551b29790061b2",
+    "url": "/sonalibitbucket1993/cakerepo.git/static/js/runtime~main.86b5c555.js"
+  },
+  {
+    "revision": "e49e9a41a37ea68a8edd",
+    "url": "/sonalibitbucket1993/cakerepo.git/static/js/main.e49e9a41.chunk.js"
+  },
+  {
+    "revision": "ba3ae591197112308b34",
+    "url": "/sonalibitbucket1993/cakerepo.git/static/js/1.ba3ae591.chunk.js"
+  },
+  {
+    "revision": "e49e9a41a37ea68a8edd",
+    "url": "/sonalibitbucket1993/cakerepo.git/static/css/main.ad1896b3.chunk.css"
+  },
+  {
+    "revision": "535b0baaf204429b205db9550eb463f4",
+    "url": "/sonalibitbucket1993/cakerepo.git/index.html"
+  }
+];
